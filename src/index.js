1cd96958672e6832401bcbcbd9a6f018aa290e66
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-07-25 20:56:49 +0530

    utils change

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+import express from 'express'
+
+dotenv.config({
+    path: './env'
+})
+
+const app = express()
+
+
+connectDB()
+.then(
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(` Server is running at port : ${process.env.PORT}`)
+    })
+)
+.catch((erro)=>{
+    console.log('MOONGO db connection fails !!! ',erro)
+})
+
